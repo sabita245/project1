96f5c7bd7e50c3e2f656088fdc48405bed36f195
sabita245 <86408787+sabita245@users.noreply.github.com>
2021-08-19 15:13:02 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -25,6 +25,6 @@ sudo apt-get update && sudo apt-get install -y apt-transport-https curl
 sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
 echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 sudo apt-get update
-#sudo apt-get install -y kubelet kubeadm kubectl
-sudo apt-get install -y kubelet=1.21.1-00 kubeadm=1.21.1-00 kubectl=1.21.1-00 #--allow-change-held-packages
+sudo apt-get install -y kubelet=1.20.0-00 kubeadm=1.20.0-00 kubectl=1.20.0-00
+#sudo apt-get install -y kubelet=1.21.1-00 kubeadm=1.21.1-00 kubectl=1.21.1-00 #--allow-change-held-packages
 sudo apt-mark hold kubelet kubeadm kubectl
